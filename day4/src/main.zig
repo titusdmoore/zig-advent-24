a1a736033f7abfd253427adf42552c22ae06e774
Titus Moore <titusdmoore@gmail.com>
2024-12-10 17:19:48 -0500

    Just messing around with day4

--- day4/src/main.zig
+++ day4/src/main.zig
@@ -10,43 +10,115 @@ const ReadEnum = enum {
     M,
     A,
     S,
-};
 
-const StateRead = struct {
-    state: ReadEnum,
+    pub fn getNext(self: ReadEnum) ReadEnum {
+        return switch (self) {
+            ReadEnum.None => ReadEnum.X,
+            ReadEnum.X => ReadEnum.M,
+            ReadEnum.M => ReadEnum.A,
+            ReadEnum.A => ReadEnum.S,
+            ReadEnum.S => ReadEnum.None,
+        };
+    }
 
-    pub fn readChar(self: *StateRead, char: u8) bool {
-        switch (self.state) {
-            ReadEnum.None => if (char == 'X') self.state == ReadEnum.X,
-            ReadEnum.X => if (char == 'M') self.state == ReadEnum.M,
-            ReadEnum.M => if (char == 'A') self.state == ReadEnum.A,
-            ReadEnum.A => if (char == 'S') self.state == ReadEnum.S,
-            ReadEnum.S => {
-                return true;
-            },
-        }
-
-        return false;
+    // Bitwise Flags
+    // 00 - Format
+    // 01 - Valid for next
+    // 11 - Valid and complete
+    pub fn validForNextEnum(self: ReadEnum, input: u8) u8 {
+        return switch (self) {
+            ReadEnum.None => if (input == 'X') 0b01 else 0b00,
+            ReadEnum.X => if (input == 'M') 0b01 else 0b00,
+            ReadEnum.M => if (input == 'A') 0b01 else 0b00,
+            ReadEnum.A => if (input == 'S') 0b11 else 0b00,
+            ReadEnum.S => 0b00,
+        };
     }
 };
 
-pub fn tryReadWord(level: usize) bool {
-    if (level > 3) return false;
+// const StateRead = struct {
+//     state: ReadEnum,
+//     tries: u8,
+//
+//     pub fn readChar(self: *StateRead, char: u8) bool {
+//         switch (self.state) {
+//             ReadEnum.None => if (char == 'X') self.state == ReadEnum.X,
+//             ReadEnum.X => if (char == 'M') self.state == ReadEnum.M,
+//             ReadEnum.M => if (char == 'A') self.state == ReadEnum.A,
+//             ReadEnum.A => if (char == 'S') self.state == ReadEnum.S,
+//             ReadEnum.S => {
+//                 return true;
+//             },
+//         }
+//
+//         return false;
+//     }
+// };
 
-    _ = tryReadWord(level + 1);
-    std.debug.print("{d}\n", .{level});
+// fn test read
+// input: ReadEnum
+// input: pos
+// if pos == validNext WHERE validNext == S
+// if pos != validNext for ReadEnum || pos out of range return (return false?)
+// define out bool
+//
+// NOTE: WHEN RECURSE WE USE NEXT STATE
+// out || recurse for up right
+// out || recurse for up middle
+// out || recurse for up left
+// out || recurse for left
+// out || recurse for right
+// out || recurse for down right
+// out || recurse for down middle
+// out || recurse for down left
+pub fn readText() void {}
+
+pub fn readTextHelper(readState: ReadEnum, ptr: usize, rowLength: usize, text: []u8) bool {
+    if (readState.validForNextEnum(text[ptr]) == 0) return false;
+
+    var out: bool = false;
+
+    out = true;
+
     // Up Left
+    out = out or readTextHelper(readState.getNext(), ((ptr - rowLength) % rowLength) - 1, rowLength, text);
     // Up Middle
+    out = out or readTextHelper(readState.getNext(), (ptr - rowLength) % rowLength, rowLength, text);
     // Up Right
-    // Right
-    // Left
-    // Bottom Left
-    // Bottom Middle
-    // Bottom Right
+    out = out or readTextHelper(readState.getNext(), ((ptr - rowLength) % rowLength) + 1, rowLength, text);
 
-    return false;
+    // Left
+    out = out or readTextHelper(readState.getNext(), ptr - 1, rowLength, text);
+    // Right
+    out = out or readTextHelper(readState.getNext(), ptr + 1, rowLength, text);
+
+    // Down Left
+    out = out or readTextHelper(readState.getNext(), ((ptr + rowLength) % rowLength) - 1, rowLength, text);
+    // Down Middle
+    out = out or readTextHelper(readState.getNext(), (ptr + rowLength) % rowLength, rowLength, text);
+    // Down Right
+    out = out or readTextHelper(readState.getNext(), ((ptr + rowLength) % rowLength) + 1, rowLength, text);
+
+    return out or readState.validForNextEnum(text[ptr]) == 7;
 }
 
+// pub fn tryReadWord(readState: *StateRead) bool {
+//     if (readState.tries == 3) return false;
+//
+//     // _ = tryReadWord(level + 1);
+//     // std.debug.print("{d}\n", .{level});
+//     // Up Left
+//     // Up Middle
+//     // Up Right
+//     // Right
+//     // Left
+//     // Bottom Left
+//     // Bottom Middle
+//     // Bottom Right
+//
+//     return false;
+// }
+//
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
